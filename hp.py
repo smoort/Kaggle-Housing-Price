a8bfc460f43bdf5a60e05b497715cd72e37bbf72
smoort <sarav_m@hotmail.com>
2019-03-02 23:18:01 +0530

    tuning model

--- hp.py
+++ hp.py
@@ -4,6 +4,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.impute import SimpleImputer
 print("Modules imported")
 
 # Path of the file to read. We changed the directory structure to simplify submitting to a competition
@@ -12,6 +13,8 @@ iowa_file_path = 'data/train.csv'
 home_data = pd.read_csv(iowa_file_path)
 # Create target object and call it y
 y = home_data.SalePrice
+
+'''
 # Create X
 features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
 X = home_data[features]
@@ -41,10 +44,11 @@ rf_val_predictions = rf_model.predict(val_X)
 rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
 
 print("Validation MAE for Random Forest Model2: {:,.0f}".format(rf_val_mae))
+'''
 
 # Create new X
 
-#new_X = home_data.drop(['Id', 'SalePrice'],axis=1)
+new_X = home_data.drop(['Id', 'SalePrice'],axis=1)
 #features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd', 'MSZoning', 'Street', 'Neighborhood']
 #new_X = home_data[features]
 #new_X = new_X.fillna(method='ffill')
@@ -59,6 +63,41 @@ new_train_X, new_val_X, new_train_y, new_val_y = train_test_split(new_X, y, rand
 print("Data preprocessed")
 
 
+new_train_X['label'] = 1
+new_val_X['label'] = 0
+#print(new_train_X.describe)
+#print(new_val_X.describe)
+
+# Concat
+concat_df = pd.concat([new_train_X, new_val_X])
+print('after concat')
+
+# Create your dummies
+features_df = pd.get_dummies(concat_df, dummy_na=True)
+#features_df = concat_df.fillna(method='ffill')
+imputed_features_df = features_df.copy()
+print(features_df.columns)
+
+my_imputer = SimpleImputer()
+imputed_features_nd = my_imputer.fit_transform(imputed_features_df)
+imputed_features_df = pd.DataFrame(imputed_features_nd, columns=features_df.columns)
+print('after dummies')
+#imputed_features_df.colums = features_df.columns
+print(imputed_features_df.columns)
+print(imputed_features_df.describe)
+print(imputed_features_df.label)
+
+# Split your data
+train_df = imputed_features_df[imputed_features_df['label'] == 1]
+score_df = imputed_features_df[imputed_features_df['label'] == 0]
+print('after split')
+
+
+# Drop your labels
+new_train_X = train_df.drop('label', axis=1)
+new_val_X = score_df.drop('label', axis=1)
+print('after drop')
+
 # Define the model. Set random_state to 1
 rf_model = RandomForestRegressor(random_state=1)
 rf_model.fit(new_train_X, new_train_y)
@@ -70,65 +109,11 @@ rf_val_mae = mean_absolute_error(rf_val_predictions, new_val_y)
 print("Validation MAE for Random Forest Model21: {:,.0f}".format(rf_val_mae))
 
 # Define the model. Set random_state to 1
-rf_model = RandomForestRegressor(n_estimators=2000, random_state=1)
+rf_model = RandomForestRegressor(n_estimators=20, random_state=1)
 rf_model.fit(new_train_X, new_train_y)
-print("Training1 completed")
+print("Training2 completed")
 
 rf_val_predictions = rf_model.predict(new_val_X)
 rf_val_mae = mean_absolute_error(rf_val_predictions, new_val_y)
 
 print("Validation MAE for Random Forest Model22: {:,.0f}".format(rf_val_mae))
-
-'''
-
-
-# Code you have previously used to load data
-import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import train_test_split
-
-print("Modules imported")
-
-# Read the file with training data
-iowa_file_path = 'data/train.csv'
-rf_model_on_full_data = pd.read_csv(iowa_file_path)
-print(rf_model_on_full_data.head())
-
-# Create target object and call it y
-y = rf_model_on_full_data.SalePrice
-
-# Create training data X
-#features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
-#X = rf_model_on_full_data[features]
-X = rf_model_on_full_data.drop(['Id','SalePrice'], axis=1)
-print(X.head())
-
-# fit rf_model_on_full_data on all data from the 
-
-# Specify Model
-iowa_model = RandomForestRegressor(random_state=1)
-# Fit Model
-#iowa_model.fit(X, y)
-
-print("Training completed")
-
-# path to file you will use for predictions
-test_data_path = 'data/test.csv'
-
-# read test data file using pandas
-test_data = pd.read_csv(test_data_path)
-
-# create test_X which comes from test_data but includes only the columns you used for prediction.
-# The list of columns is stored in a variable called features
-#test_X = test_data[features]
-test_X = test_data.drop(['Id'], axis=1)
-
-# make predictions which we will submit. 
-test_preds = iowa_model.predict(test_X)
-
-print("Validation Completed")
-
-print("Adios Amigo")
-
-'''
---
+++ xg_hp.py
@@ -0,0 +1,70 @@
+# Code you have previously used to load data
+import pandas as pd
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from xgboost import XGBRegressor
+print("Modules imported")
+
+# Path of the file to read. We changed the directory structure to simplify submitting to a competition
+iowa_file_path = 'data/train.csv'
+home_data = pd.read_csv(iowa_file_path)
+
+# Create target object and call it y
+y = home_data.SalePrice
+
+# Create X
+X = home_data.drop(['Id', 'SalePrice'],axis=1)
+
+# Split into validation and training data
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+train_X['label'] = 1
+val_X['label'] = 0
+
+# Concat
+concat_df = pd.concat([train_X, val_X])
+print('after concat')
+
+# Create your dummies
+features_df = pd.get_dummies(concat_df, dummy_na=True)
+#features_df = concat_df.fillna(method='ffill')
+print('after dummies')
+
+# Fill holes by imputation
+imputed_features_df = features_df.copy()
+my_imputer = SimpleImputer()
+imputed_features_nd = my_imputer.fit_transform(imputed_features_df)
+imputed_features_df = pd.DataFrame(imputed_features_nd, columns=features_df.columns)
+
+# Split your data
+train_df = imputed_features_df[imputed_features_df['label'] == 1]
+score_df = imputed_features_df[imputed_features_df['label'] == 0]
+print('after split')
+print("Data preprocessed")
+
+# Drop your labels
+train_X = train_df.drop('label', axis=1)
+val_X = score_df.drop('label', axis=1)
+print('after drop')
+
+# Define the model. Set random_state to 1
+rf_model = XGBRegressor()
+rf_model.fit(train_X, train_y, verbose=False)
+print("Training1 completed")
+
+rf_val_predictions = rf_model.predict(val_X)
+rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
+
+print("Validation MAE for Random Forest Model21: {:,.0f}".format(rf_val_mae))
+
+# Define the model.
+rf_model = XGBRegressor(n_estimators=1000)
+rf_model.fit(train_X, train_y, early_stopping_rounds=50, 
+             eval_set=[(val_X, val_y)], verbose=False)
+print("Training2 completed")
+
+rf_val_predictions = rf_model.predict(val_X)
+rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
+
+print("Validation MAE for Random Forest Model22: {:,.0f}".format(rf_val_mae))
